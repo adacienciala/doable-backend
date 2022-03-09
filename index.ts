f5962590452ba0ccdd97b98370bbf9a6dd904a4e
adacienciala <adacien@o2.pl>
2022-03-09 01:31:54 +0100

    Added login

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import {Db, MongoClient} from 'mongodb'
 import express from 'express'
 import dotenv from 'dotenv'
+import * as emailValidator from 'email-validator'
+import * as bcrypt from 'bcrypt'
+import * as hash from 'password-hash'
+import { uuidv4} from 'uuid'
 
 dotenv.config()
 
@@ -19,13 +23,51 @@ async function connectToDb(): Promise<{
 (async () => {
   const { err, db } = await connectToDb();
   
-  if (err) throw err;
+  if (err || !db) throw err;
   
   const app = express();
   app.use(express.json());
   
   app.get('/', (req, res) => {
     res.send('hey babe');
+  });
+
+  app.post('/login', async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+      return res.json({
+        success: false,
+        msg: 'incomplete query'
+      });
+    }
+    if (!emailValidator.validate(req.body.email)) {
+      return res.json({
+        success: false,
+        msg: 'invalid email address'
+      });
+    }
+    const user = await db.collection('accounts').findOne({email: req.body.email});
+    if (!user) {
+      return res.json({
+        success: false,
+        msg: 'incorrect email'
+      });
+    }
+    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!passwordMatch) {
+      return res.json({
+        success: false,
+        msg: 'incorrect password'
+      });
+    }
+    const token = uuidv4();
+    const tokenSelector = uuidv4();
+    const hashedToken = hash.generate(token);
+    await db.collection('accounts').updateOne({_id: user._id}, {$set: {
+      token: hashedToken,
+      tokenTimestamp: Date.now(),
+      tokenSelector: tokenSelector
+    }})
+    res.json({success: true, token: token, selector: tokenSelector});
   });
   
   const PORT = process.env.PORT || 3000;
