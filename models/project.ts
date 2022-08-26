1e73f70eafdd786d9944a71f20735cd85d5b2dbc
adacienciala <adacien@o2.pl>
2022-08-26 13:44:28 +0200

    Add project model schema

---
+++ models/project.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export interface IProject {
+  projectId: string;
+  name: string;
+  cover: string;
+  owner: string[];
+}
+
+const ProjectSchema = new mongoose.Schema<IProject>({
+  projectId: { type: String, required: true },
+  name: { type: String, required: true },
+  cover: { type: String, required: true },
+  owner: [String],
+});
+
+export const Project = mongoose.model<IProject>(
+  "Project",
+  ProjectSchema,
+  "projects"
+);
