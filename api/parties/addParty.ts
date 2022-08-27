84df211e94a003124cf66a25eec0cd476752ba1e
adacienciala <adacien@o2.pl>
2022-08-27 21:29:35 +0200

    Add linkin users with party on party creation

--- api/parties/addParty.ts
+++ api/parties/addParty.ts
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import { IParty, Party } from "../../models/party";
+import { User } from "../../models/user";
 import { generateUniquePartyId } from "../../utils/parties";
 
 interface AddPartyBody extends IParty {}
@@ -7,16 +9,32 @@ export const addParty = async (req, res) => {
   const partyData: AddPartyBody = req.body;
   const userDoableId = req.userDoableId;
 
+  const members = [userDoableId, ...(partyData.members ?? [])];
+
+  const users = await User.find({ doableId: { $in: members } });
+  if (users.length !== members.length) {
+    return res.status(400).json({ msg: "Unknown users in members array" });
+  }
+
   const newParty = {
     partyId: await generateUniquePartyId(),
     name: partyData.name,
     description: partyData.description,
-    members: [userDoableId, ...(partyData.members ?? [])],
+    members: members,
     cover: partyData.cover,
   };
   const dbParty = await Party.create<IParty>(newParty);
 
-  // TODO: link members to party
-
-  return res.status(201).json(dbParty);
+  try {
+    users.forEach(async (user) => {
+      user.partyId = dbParty.partyId;
+      await user.save();
+    });
+    return res.status(201).json(dbParty);
+  } catch (e) {
+    if (e instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(400).json({ msg: "Not all users have been updated" });
+    }
+    return res.status(500).json({ msg: "Users couldn't be updated" });
+  }
 };
