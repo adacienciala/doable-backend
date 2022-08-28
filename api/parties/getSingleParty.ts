92d713cfcb0b001d7d8db8260970a5ab6528a27c
adacienciala <adacien@o2.pl>
2022-08-28 03:13:41 +0200

    Add more info on getParty

--- api/parties/getSingleParty.ts
+++ api/parties/getSingleParty.ts
@@ -1,4 +1,5 @@
 import { Party } from "../../models/party";
+import { User } from "../../models/user";
 
 export const getSingleParty = async (req, res) => {
   const partyId = req.params.partyId;
@@ -6,9 +7,23 @@ export const getSingleParty = async (req, res) => {
   const dbParty = await Party.findOne({
     members: userDoableId,
     partyId: partyId,
-  });
+  }).lean();
   if (!dbParty) {
     return res.status(404).json({ msg: "Party not found" });
   }
-  return res.status(200).json(dbParty);
+  const members = await User.find({ doableId: dbParty.members });
+
+  const mappedParty: any = dbParty;
+  mappedParty.members = dbParty.members.map((memberId) => {
+    const member = members.find((m) => m.doableId === memberId);
+    const newMemberData = {
+      name: member?.name,
+      surname: member?.surname,
+      statistics: member?.statistics,
+      settings: member?.settings,
+    };
+    return newMemberData;
+  });
+
+  return res.status(200).json(mappedParty);
 };
