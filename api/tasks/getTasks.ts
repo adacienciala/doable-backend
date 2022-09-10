0ab03ccd2bb974b75c6ad645beb8ac48b5144e0d
adacienciala <adacien@o2.pl>
2022-09-11 00:06:55 +0200

    Extend getTasks with tasks from projects

--- api/tasks/getTasks.ts
+++ api/tasks/getTasks.ts
@@ -1,8 +1,19 @@
+import { Project } from "../../models/project";
 import { Task } from "../../models/task";
 
 export const getTasks = async (req, res) => {
   const userDoableId = req.userDoableId;
-  const tasks = await Task.find({ owner: userDoableId, isDone: false });
+  const userPartyId = req.userPartyId;
+  const projects = await Project.find({
+    $or: [{ owner: userDoableId }, { party: userPartyId }],
+  })
+    .select({ projectId: 1 })
+    .lean();
+  const projectIds = projects.map((p) => p.projectId);
+  const tasks = await Task.find({
+    $or: [{ owner: userDoableId }, { projectId: projectIds }],
+    isDone: false,
+  }).lean();
   if (!tasks) {
     return res.status(404).json({
       msg: "could not find tasks",
